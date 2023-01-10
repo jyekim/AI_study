f92e199c64b0a5d0ec2cd486dbdb966b03e1ab9a
jyekim <121778189+jyekim@users.noreply.github.com>
2023-01-10 20:15:31 +0900

    1/10

--- keras/keras22_softmax4_fetch_covtype.py
+++ keras/keras22_softmax4_fetch_covtype.py
@@ -12,38 +12,35 @@ y = datasets['target']
 print(x.shape, y.shape)   #(581012, 54) (581012,)
 print(np.unique(y, return_counts=True))      #(array([1, 2, 3, 4, 5, 6, 7]), array([211840, 283301,  35754,   2747,   9493,  17367,  20510], dtype=int64)
 
-from tensorflow.keras.utils import to_categorical
+# from tensorflow.keras.utils import to_categorical
 
-# # Perform one-hot-encoding on the target variable 'y'
-y = to_categorical(y.reshape(-1,1))
-# # Print the new shape of the target variable 'y'
-print(y.shape)
-
-
+# # # Perform one-hot-encoding on the target variable 'y'
+# y = to_categorical(y.reshape(-1,1))
+# # # Print the new shape of the target variable 'y'
+# print(y.shape)
 
 
-# import pandas as pd                        #첫번째 방법 
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder   # Initialize the OneHotEncoder  #첫 번째 방법 
+ohe = OneHotEncoder()
+
+# Fit the OneHotEncoder on the target variable 'y'
+y = ohe.fit_transform(y.reshape(-1,1)).toarray()  
+# Print the new shape of the target variable 'y'
+print(y.shape)    #(581012, 7)     
+
+
+# import pandas as pd                        # 두 번째 방법 
 # y = pd.get_dummies(y)
 # y_np = y.values
 # print(y.shape)  # (581012, 7)
 
 
-# import pandas as pd                       #두번째 방법
+# import pandas as pd                       # 세 번째 방법
 # y = pd.get_dummies(y)
 # y_np = y.to_numpy()
 
 
-
-
-# import pandas as pd
-# from sklearn.preprocessing import OneHotEncoder   # Initialize the OneHotEncoder  #3번째 방법 
-# ohe = OneHotEncoder()
-
-# # Fit the OneHotEncoder on the target variable 'y'
-# y = ohe.fit_transform(y.reshape(-1,1)).toarray()    
-
-# # Print the new shape of the target variable 'y'
-# print(y.shape)    #(581012, 7)     
 
 # #쉐이프를 맞추는 작업 
 # #print(type(y))
@@ -58,17 +55,17 @@ print(y)
 print(y.shape)  #(581012, 7)
 
 
-x_train, x_test, y_train, y_test = train_test_split(x, y,
-                                                    shuffle=True, random_state=222, test_size=0.2, stratify=y)
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, shuffle=True, random_state=222, test_size=0.2, stratify=y)
 
 #2. 모델구성 
 model = Sequential()
 model.add(Dense(5, activation= 'relu', input_shape=(54, )))
-model.add(Dense(20, activation='relu'))
-model.add(Dense(20, activation='relu'))
-model.add(Dense(20, activation='relu'))
-model.add(Dense(20, activation='relu'))
-model.add(Dense(20, activation='relu'))
+model.add(Dense(1000, activation='relu'))
+model.add(Dense(430, activation='relu'))
+model.add(Dense(250, activation='relu'))
+model.add(Dense(73, activation='relu'))
+model.add(Dense(92, activation='relu'))
 model.add(Dense(7, activation= 'softmax'))
 
 #3.컴파일, 훈련 
@@ -78,14 +75,14 @@ model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics=['acc
 earlystopping = EarlyStopping(monitor='val_loss', mode='min', patience=10, restore_best_weights=True, verbose=1)
 import time
 start = time.time()
-model.fit(x_train, y_train, epochs=100, batch_size=128, 
+model.fit(x_train, y_train, epochs=100, batch_size=64, 
           validation_split=0.2, callbacks=[earlystopping], verbose=1)
 end = time.time()
 print('걸린시간 : ', end- start)
 
 #4. 평가 예측 
 loss, accuracy = model.evaluate(x_test, y_test)
-print('loss : ' , loss)  
+print('loss : ', loss)  
 print('accuracy : ', accuracy)                              
 
 # print(y_test[:5])                                                       
@@ -108,5 +105,21 @@ acc = accuracy_score(y_test, y_predict),
 print(acc)
 
 
+"""
+OneHotEncoder (첫번째 방법 썼을때) 
+Epoch 00051: early stopping
+걸린시간 :  155.87018036842346
+3632/3632 [==============================] - 2s 573us/step - loss: 0.5573 - accuracy: 0.7550
+loss :  0.5573306679725647
+accuracy :  0.7550407648086548
+y_pred(예측값) : [6 2 2 ... 1 0 2]
+y_test(원래값) :  [6 2 2 ... 1 0 5]
+[6 2 2 ... 1 0 2]
+(0.7550407476571173,)
 
+
+
+
+
+"""
 
